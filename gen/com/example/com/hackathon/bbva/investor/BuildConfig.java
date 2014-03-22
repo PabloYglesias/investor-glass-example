b3557da70b1a1c020300e5c23411e7d297ff5623
PabloYglesias <contacto@pabloyglesias.com>
2014-03-22 11:47:37 +0100

    Goes Investor

---
+++ gen/com/example/com/hackathon/bbva/investor/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.com.hackathon.bbva.investor;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
